bf239bbff93d0d5e26c8a97b58124b47ba741627
Cristopher Caceres <cristopher@frubana.co>
2020-05-15 16:53:56 -0500

    work in progress

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Work in progress</p>
+        <p>Work in progress.</p>
       </header>
     </div>
   );
